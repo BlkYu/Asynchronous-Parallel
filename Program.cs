7b5a26bb93b09652b4371be4ae4bc497c72206be
YU <ankoanko3.14@gmail.com>
2023-11-04 15:20:49 +0900

    ラムダ式をメソッド化

--- Program.cs
+++ Program.cs
@@ -5,19 +5,22 @@
         //メインスレッド
         static void Main(string[] args)
         {
-            Task.Factory.StartNew(() =>
-            {
-                //別スレッド
-                for (int i = 0; i < 1024; i++)
-                {
-                    Console.Write("1");
-                }
-            });
+            //別スレッド
+            Task.Run(new Action(WriteOne));
+            //Task.Run(WriteOne);
 
             //メインスレッド
             for (int i = 0; i < 1024; i++)
             {
                 Console.Write("2");
+            }
+        }
+
+        private static void WriteOne()
+        {
+            for (int i = 0; i < 1024; i++)
+            {
+                Console.Write("1");
             }
         }
     }
