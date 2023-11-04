bc6c91c29171da8e5e82866a63a70574fcbbccbb
YU <ankoanko3.14@gmail.com>
2023-11-04 14:49:22 +0900

    シンプルスレッド_Taskクラスで記述

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 1024; i++)
+                {
+                    Console.Write("1");
+                }
+                
+            });
+            for (int i = 0; i < 1024; i++)
+            {
+                Console.Write("2");
+            }
         }
     }
 }
