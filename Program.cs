00097779a87a2b2abe99c927d41f9d625d442d02
YU <ankoanko3.14@gmail.com>
2023-11-04 17:05:00 +0900

    戻り値

--- Program.cs
+++ Program.cs
@@ -2,26 +2,22 @@
 {
     internal class Program
     {
-        //メインスレッド
         static void Main(string[] args)
         {
-            //別スレッド
-            var msg = "1";
-            Task.Run(() => proc(msg));
-
-            Thread.Sleep(10);
-            msg = "0";
-
-            //メインスレッド
-            proc("2");
-        }
-
-        private static void proc(object param)
-        {
-            for (int i = 0; i < 1024; i++)
+            Task<int> task = Task.Run(new Func<int>(() =>
+            //var task = Task.Run(() =>
             {
-                Console.Write(param);
-            }
+                var sum = 0;
+                for (int i = 0; i < 16; i++)
+                {
+                    sum += i;
+                    Console.Write($"{i}+");
+                }
+                Console.Write("\b \b");
+                return sum;
+            }));
+            Console.WriteLine($" = {task.Result}");
+
         }
     }
 }
