b9817769ce80144b4b8463379312f139cd48c8fd
YU <ankoanko3.14@gmail.com>
2023-11-04 14:58:07 +0900

    Task.Factory.StartNewで記述

--- Program.cs
+++ Program.cs
@@ -2,16 +2,19 @@
 {
     internal class Program
     {
+        //メインスレッド
         static void Main(string[] args)
         {
-            Task.Run(() =>
+            Task.Factory.StartNew(() =>
             {
+                //別スレッド
                 for (int i = 0; i < 1024; i++)
                 {
                     Console.Write("1");
                 }
-                
             });
+
+            //メインスレッド
             for (int i = 0; i < 1024; i++)
             {
                 Console.Write("2");
