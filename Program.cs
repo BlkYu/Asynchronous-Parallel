951b91636d052d5beb4e9cb9f986452d6d6bdae3
YU <ankoanko3.14@gmail.com>
2023-11-05 20:19:37 +0900

    Task戻り値

--- Program.cs
+++ Program.cs
@@ -2,22 +2,16 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] _)
         {
-            Task<int> task = Task.Run(new Func<int>(() =>
-            //var task = Task.Run(() =>
-            {
-                var sum = 0;
-                for (int i = 0; i < 16; i++)
-                {
-                    sum += i;
-                    Console.Write($"{i}+");
-                }
-                Console.Write("\b \b");
-                return sum;
-            }));
-            Console.WriteLine($" = {task.Result}");
+            string str = await Method();
+            Console.WriteLine($"{str}");
+        }
 
+        private static Task<string> Method()
+        {
+            Thread.Sleep( 1000 );
+            return "messages";
         }
     }
 }
