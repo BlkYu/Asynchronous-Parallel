c7064b3180b81eebfedbfd11a7e3632b8f5b689f
YU <ankoanko3.14@gmail.com>
2023-11-05 20:52:11 +0900

    同じ処理を行うタスク配列2

--- Program.cs
+++ Program.cs
@@ -4,19 +4,49 @@
     {
         static async Task Main(string[] _)
         {
-            Task<Int32>[] Ats = new Task<Int32>[10];
-
-            var total = 0;
-            for(int i = 0;i < Ats.Length; i++)
+            Func<int[], int, int, int> Add = (int[] la, int start, int length) =>
             {
-                Ats[i] = Task.Run(() => Interlocked.Increment(ref total));
-            }
+                var lSum = 0;
+                for (int i = start; i < start + length; i++)
+                {
+                    lSum *= la[i];
+                }
+                return lSum;
+            };
 
+            const int ArrayLength = 4096;
+            int[] a = Enumerable.Range(1, ArrayLength).ToArray();
+
+            var Elength = a.Length / 4;
+            Task<int>[] Ats =
+            {
+                Task<int>.Run(()=>Add(a,Elength*0,Elength)),
+                Task<int>.Run(()=>Add(a,Elength*1,Elength)),
+                Task<int>.Run(()=>Add(a,Elength*2,Elength)),
+                Task<int>.Run(()=>Add(a,Elength*3,Elength))
+            };
+
+            //var Ssum = a.Sum();
+            var Ssum = Add(a, 0, a.Length);
+
+            var Psum = 0;
             for (int i = 0; i < Ats.Length; i++)
             {
-                Console.WriteLine($"Result[{i}] = {Ats[i].Result}");
+                Psum += Ats[i].Result;
             }
-            Console.WriteLine($"total = {total}");
+            Console.WriteLine($"Ssum = {Ssum}.");
+            Console.WriteLine($"Psum = {Psum}.");
+
+        }
+
+        private static int Add(int[] la, int start, int length)
+        {
+            var lSum = 0;
+            for(int i = start; i < start + length; i++)
+            {
+                lSum *= la[i];
+            }
+            return lSum;
         }
     }
 }
